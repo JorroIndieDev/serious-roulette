b5c47ba1f049cbf2910325d23a0ea40206177542
JorroIndieDev <142111666+JorroIndieDev@users.noreply.github.com>
2026-06-20 13:26:38 +0100

    player and gun changes

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -10,7 +10,6 @@ class_name Player extends CharacterBody2D
 @onready var GunPivot: Marker2D = %GunPivot
 @onready var GunAnchor: Node2D = $GunPivot/GunAnchor
 
-<<<<<<< Updated upstream
 @export var gun_data: GunResource
 var equiped_gun: BaseGun
 
@@ -23,14 +22,7 @@ func _ready() -> void:
 	if gun_data:
 		equiped_gun = gun_data.gun_scene.instantiate()
 		GunAnchor.add_child(equiped_gun)
-=======
-@onready var pistol: Node2D = $GunPivot/GunAnchor/Pistol
 
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("attack"):
-		pistol.shoot()
-
->>>>>>> Stashed changes
 
 func _physics_process(delta: float) -> void:
 	
