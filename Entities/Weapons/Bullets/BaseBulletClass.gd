0ec6a04388a31979d8262c175c6af4cc655e3964
JorroIndieDev <142111666+JorroIndieDev@users.noreply.github.com>
2026-06-20 18:16:26 +0100

    added base bullet class

---
+++ Entities/Weapons/Bullets/BaseBulletClass.gd
@@ -0,0 +1,20 @@
+class_name BaseBullet extends Node2D
+
+@export var hitbox: HitBoxComponent
+var bullet_resource: BulletResource
+var attack: Attack
+var direction: Vector2
+
+func _ready() -> void:
+	if hitbox:
+		hitbox.connect("area_entered", _on_hit)
+
+func _process(delta: float) -> void:
+	self.global_position += direction * bullet_resource.bullet_speed * delta
+
+func _on_hit(area: Area2D) -> void:
+	if area.owner is BaseBullet:
+		return # The fix... 
+	if area is HitBoxComponent:
+		area.damage(attack)
+	self.queue_free()
