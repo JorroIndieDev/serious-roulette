a6df12857f4b62dce79e8333c4c83664ce789276
JorroIndieDev <142111666+JorroIndieDev@users.noreply.github.com>
2026-06-20 18:16:26 +0100

    added base bullet class

--- Entities/Weapons/HeavyPistol/heavy_pistol.gd
+++ Entities/Weapons/HeavyPistol/heavy_pistol.gd
@@ -4,7 +4,7 @@ func _shoot() -> void:
 
 	var bullet_data := _apply_bullet_modifiers(gun_data.loaded_bullet)
 	
-	var bullet: BasicBullet = bullet_data.bullet_scene.instantiate()
+	var bullet: BaseBullet = bullet_data.bullet_scene.instantiate()
 	bullet.bullet_resource = bullet_data
 	bullet.attack = _calculate_attack(bullet_data)
 	GameManager.ProjectileContainer.call_deferred("add_child", bullet)
