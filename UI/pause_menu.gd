7047e0260e63efa69b9b61bf30b30fc36c9feb53
JorroIndieDev <142111666+JorroIndieDev@users.noreply.github.com>
2026-06-21 19:27:56 +0100

    pause menu

---
+++ UI/pause_menu.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var menu_ui: Control = $menu_UI
+
+func _ready() -> void:
+	menu_ui.visible=false
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+		
+func toggle_pause():
+	var is_paused = not get_tree().paused
+	get_tree().paused = is_paused
+	
+	menu_ui.visible = is_paused
+
+
+func _on_continue_pressed() -> void:
+	toggle_pause()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
